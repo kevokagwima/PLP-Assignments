3f42c23351b0a50bf3bde5b00940ea5719db65c1
kevin <you@example.com>
2024-03-18 10:15:11 +0300

    Updated Files

---
+++ list.py
@@ -0,0 +1,11 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+another_list = [50,60,70]
+my_list = my_list + another_list
+my_list.pop()
+my_list.sort()
+print(my_list)
