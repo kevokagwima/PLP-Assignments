d32c00c176547743614ef5ae753bcc3bd27cf186
kevin <you@example.com>
2024-03-18 09:57:50 +0300

    updated files

---
+++ Assignment 1/user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+print(f"Hello, {name}. You are {age} years old and live in {location}")
